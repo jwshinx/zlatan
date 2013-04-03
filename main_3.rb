cf0c3aa8dd1a36611806cd53d6b733022207cc7a
Joel Shin <jwshin@gmail.com>
2013-04-03 14:39:03 -0700

    filenames entered as args

--- main_3.rb
+++ main_3.rb
@@ -3,6 +3,14 @@ $LOAD_PATH << File.expand_path('../lib', __FILE__)
 require "rexml/document"
 require 'xml_ripper'
 
+raise %q(
+
+           Error. Provide two arguments: <ripper file> <xml file>
+           EG: fix_cities.ripper cities.xml 
+        ) if ARGV[0].nil? || ARGV[1].nil?
+
 xr = XmlRipper.new
-xr.initialize_from_file( 'fix_cities.ripper' )
-xr.run( 'cities.xml' )
+xr.initialize_from_file( ARGV[0] )
+xr.run( ARGV[1] )
+
+
