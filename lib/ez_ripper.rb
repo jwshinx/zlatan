8b77547733d8a35e2e984b8c17258d19a049ed43
Joel Shin <jwshin@gmail.com>
2013-04-03 17:39:56 -0700

    rfactrd

--- lib/ez_ripper.rb
+++ lib/ez_ripper.rb
@@ -22,13 +22,22 @@ class EzRipper
   return if tokens.empty?
 
   case tokens.first
+  when '#'
+   # dont do anything bc its a comment
   when 'print'
+   raise "Expected print <xpath>" unless tokens.size == 2
    @r.on_path( tokens[1] ) { |e| puts e.text }
   when 'delete'
+   raise "Expected delete <xpath>" unless tokens.size == 2
    @r.on_path( tokens[1] ) { |e| e.remove }
   when 'replace'
+   raise "Expected replace <xpath> <value>" unless tokens.size == 3
    @r.on_path( tokens[1] ) { |e| e.text = tokens[2] }
+  when 'uppercase'
+   raise "Expected uppercase <xpath>" unless tokens.size == 2 
+   @r.on_path( tokens[1] ) { |e| e.text = e.text.upcase }
   when 'print_document'
+   raise "Expected print_document" unless tokens.size == 1 
    @r.after { |doc| puts doc }
   else
    raise "Unknown keyword: #{tokens.first}"
